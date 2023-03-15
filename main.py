df5f697107db0f053c8dc64ac63d468aeef2d5ff
sebferrer <sebastien@sebferrer.fr>
2023-03-15 19:55:17 +0000

    commands handling Signed-off-by: sebferrer <sebferrer@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
-import discord
 from decouple import config
+import discord
+from discord import app_commands
 
 intents = discord.Intents.default()
-intents.message_content = True
-
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+
+@tree.command(name="help", description="My first application Command", guild=discord.Object(id=config('GUILD_ID')))
+async def help_command(interaction):
+    response = 'Here are the available commands:\n\n/help - Shows a list of available commands\n'
+    await interaction.response.send_message(response)
+
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    await tree.sync(guild=discord.Object(id=config('GUILD_ID')))
+    print("Ready!")
+
+
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     if message.author == client.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if str(message.author.id) == config('KIMIDA_ID'):
+        await message.channel.send('Miaou!')
+        reaction = '🐱'
+        await message.add_reaction(reaction)
+
+    await client.process_commands(message)
+
 
 client.run(config('TOKEN'))
